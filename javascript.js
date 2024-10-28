3c5ff7af32d57db02152ea4bc9dfcd3ec351ac93
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2024-10-28 12:45:31 +0300

    Add Basic Aruthmetic Operations
    These arithmetic operations are found on basic
    calculators

---
+++ javascript.js
@@ -0,0 +1,20 @@
+// Basic math operations of a regular calculator
+function add (first, second)
+{
+    return first + second;
+}
+
+function subtract (first, second)
+{
+    return first - second;
+}
+
+function multiply (first, second)
+{
+    return first * second;
+}
+
+function divide (first, second)
+{
+    return first / second;
+}
